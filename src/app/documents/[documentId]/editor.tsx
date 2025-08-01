61a9f56e1fa88f7c7c32bf6f9d5187825148e37e
Likhith Raju P <likhithraju0@gmail.com>
2025-08-01 22:35:37 +0530

    fix: correct formatting in the Editor component return statement

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -14,7 +14,7 @@ export const Editor = () => {
         extensions: [StarterKit],
         content: '<p>Hello World</p>'
     })
-    return ( 
+    return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print: overflow-visible print:bg-white'>
            <div className='min-w-flex flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
             <EditorContent editor={editor} />
