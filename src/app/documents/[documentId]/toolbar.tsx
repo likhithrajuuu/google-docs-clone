f89e887772e4051bd185d1f8ae741d9f911d2097
Likhith Raju P <likhithraju0@gmail.com>
2025-08-04 07:41:17 +0530

    feat: add Heading Level button to the toolbar
    - Introduced a new HeadingLevelButton component for selecting text heading levels.
    - Integrated heading level options (from Normal Text to Heading 5) with dynamic styling.
    - Updated the Toolbar component to include the new Heading Level button alongside the Font Family button.

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -28,6 +28,94 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { type Level } from "@tiptap/extension-heading";
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    {
+      label: "Normal Text",
+      value: 0,
+      fontSize: "16px",
+    },
+    {
+      label: "Heading 1",
+      value: 1,
+      fontSize: "32px",
+    },
+    {
+      label: "Heading 2",
+      value: 2,
+      fontSize: "24px",
+    },
+    {
+      label: "Heading 3",
+      value: 3,
+      fontSize: "20px",
+    },
+    {
+      label: "Heading 4",
+      value: 4,
+      fontSize: "18px",
+    },
+    {
+      label: "Heading 5",
+      value: 5,
+      fontSize: "16px",
+    },
+  ];
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive(`heading`, { level })) {
+        return `Heading ${level}`;
+      }
+    }
+
+    return "Normakl Text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+          <span className="truncate">
+            {getCurrentHeading() || "Normal Text"}
+          </span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            style={{ fontSize }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              (value === 0 && !editor?.isActive("heading")) ||
+                (editor?.isActive("heading", { level: value }) &&
+                  "bg-neutral-200/80")
+            )}
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor
+                  ?.chain()
+                  .focus()
+                  .toggleHeading({ level: value as Level })
+                  .run();
+              }
+            }}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const FontFamilyButton = () => {
   const { editor } = useEditorStore();
@@ -51,7 +139,7 @@ const FontFamilyButton = () => {
     },
     {
       label: "Georgia",
-      value: "Georgia"
+      value: "Georgia",
     },
   ];
 
@@ -67,14 +155,15 @@ const FontFamilyButton = () => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
         {fonts.map(({ label, value }) => (
-          <button 
-          key={value} 
-          className={cn(
-            "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
-            editor?.getAttributes("textStyle").fontFamily === value && "bg-neutral-200/80"
-          )}
-          style={{ fontFamily: value }}
-          onClick = {() => editor?.chain().focus().setFontFamily(value).run()}
+          <button
+            key={value}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              editor?.getAttributes("textStyle").fontFamily === value &&
+                "bg-neutral-200/80"
+            )}
+            style={{ fontFamily: value }}
+            onClick={() => editor?.chain().focus().setFontFamily(value).run()}
           >
             <span className="text-sm">{label}</span>
           </button>
@@ -165,8 +254,6 @@ const Toolbar = () => {
           );
         },
       },
-      
-      
     ],
     [
       {
@@ -218,7 +305,7 @@ const Toolbar = () => {
         icon: RemoveFormattingIcon,
         onClick: () => editor?.chain().focus().unsetAllMarks().run(),
       },
-    ]
+    ],
   ];
 
   return (
@@ -226,17 +313,18 @@ const Toolbar = () => {
       {sections[0].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-        <FontFamilyButton />
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <FontFamilyButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Font Size */}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+      <HeadingLevelButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Heading */}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {sections[1].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
