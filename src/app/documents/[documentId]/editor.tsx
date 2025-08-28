2656d677624e71feb6071cfc009291f8f6a8a056
Likhith Raju P <likhithraju0@gmail.com>
2025-08-28 19:11:39 +0530

    feat: integrate react-icons and add Ruler component to editor
    - Added `react-icons` library to project dependencies for consistent icon usage.
    - Introduced `Ruler` component and rendered it in the editor layout.
    - Updated editor configuration with `immediatelyRender: false` for enhanced performance.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,10 +20,12 @@ import Highlight from "@tiptap/extension-highlight";
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from './ruler';
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
   const editor = useEditor({
+      immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -111,6 +113,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:overflow-visible print:bg-white">
+        <Ruler />
       <div className="flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
