ca7a5ddc6f31840abd87081bfd94155384d67263
Likhith Raju P <likhithraju0@gmail.com>
2025-09-05 22:32:54 +0530

    feat: add draggable marker indicator to Ruler component

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -116,6 +116,16 @@ const Marker = ({
      onDoubleClick = {onDoubleClick}
     >
         <FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2 group-hover:fill-blue-600"/>
+        <div className="absolute left-1/2 top-4 transform -translate-x-1/2" 
+            style={{
+                height: "100vh",
+                width: "1px",
+                transform: "scaleX(0.5)",
+                backgroundColor: "#3b72f6",
+                display: isDragging ? "block" : "none",
+
+            }}
+        />
     </div>
     );
 };
